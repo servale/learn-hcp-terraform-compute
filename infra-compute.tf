524533d0ac2202a038467d9e3804cd60689378ea
servale <38044659+servale@users.noreply.github.com>
2025-12-03 22:32:05 -0500

    testing tfe_outputs data

--- infra-compute.tf
+++ infra-compute.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "vm1" {
   name                = "pip-${var.application_name}-${var.environment_name}-vm1"
-  location            = local.rg_main_location
-  resource_group_name = local.rg_main_name
+  location            = data.tfe_outputs.learn-hcp-terraform.values.rg_main_location
+  resource_group_name = data.tfe_outputs.learn-hcp-terraform.values.rg_main_name
   allocation_method   = "Static"
 }
 
--- main.tf
+++ main.tf
@@ -2,7 +2,9 @@ data "tfe_outputs" "learn-hcp-terraform" {
   workspace    = var.workspace_name
   organization = var.organization_name
 }
+/*
 locals {
   rg_main_name     = data.tfe_outputs.learn-hcp-terraform.values.rg_main_name
   rg_main_location = data.tfe_outputs.learn-hcp-terraform.values.rg_main_location
 }
+*/
